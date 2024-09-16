63b6ad03cf323fb24b6768c95a55ada2af567a4b
omar344 <3marabadi@gmail.com>
2024-09-16 15:58:27 +0300

    update

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { useSpring, animated } from "@react-spring/web";
+import { animated, useSpring } from "@react-spring/web";
 
 const achievementsList = [
   {
@@ -23,37 +23,29 @@ const achievementsList = [
   },
 ];
 
-const AnimatedNumber = ({ value }) => {
-  const { number } = useSpring({
-    from: { number: 0 },
-    number: value,
-    config: { mass: 1, tension: 140, friction: 100 },
-  });
-
-  return (
-    <animated.span>
-      {number.to((n) => Math.floor(n).toLocaleString("en-US"))}
-    </animated.span>
-  );
-};
-
 const AchievementsSection = () => {
   return (
     <div className="py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
       <div className="sm:border-[#33353F] sm:border rounded-md py-8 px-16 flex flex-col sm:flex-row items-center justify-between">
-        {achievementsList.map((achievement, index) => (
-          <div
-            key={index}
-            className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
-          >
-            <h2 className="text-white text-4xl font-bold flex flex-row">
-              {achievement.prefix}
-              <AnimatedNumber value={achievement.value} />
-              {achievement.postfix}
-            </h2>
-            <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
-          </div>
-        ))}
+        {achievementsList.map((achievement, index) => {
+          const props = useSpring({ number: achievement.value, from: { number: 0 } });
+          
+          return (
+            <div
+              key={index}
+              className="flex flex-col items-center justify-center mx-4 my-4 sm:my-0"
+            >
+              <h2 className="text-white text-4xl font-bold flex flex-row">
+                {achievement.prefix}
+                <animated.span className="text-white text-4xl font-bold">
+                  {props.number.to(n => Math.floor(n)).toLocaleString()}
+                </animated.span>
+                {achievement.postfix}
+              </h2>
+              <p className="text-[#ADB7BE] text-base">{achievement.metric}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
