fc6de014bb8246abaafa53737667896eb767c5cb
omar344 <3marabadi@gmail.com>
2024-09-20 02:52:51 +0300

    update

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -142,7 +142,7 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/omar_.jpg" width={500} height={500} />
+        <Image src="/images/omar_.png" width={500} height={500} />
         <div className="mt-4 md:mt-7 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4 mt-1">About Me</h2>
           <p className="text-base lg:text-lg">
