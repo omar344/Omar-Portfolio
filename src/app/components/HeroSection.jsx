756dc80a1f4b620be2ffb4722783ff1669f2b7bf
omar344 <3marabadi@gmail.com>
2024-09-20 11:52:37 +0300

    update

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -48,7 +48,7 @@ const HeroSection = () => {
               Hire Me
             </Link>
             <Link
-              href="https://drive.google.com/drive/folders/1r_jc1T-6fEsSgFmV6OqbsZxvgG5W_rna"
+              href="https://drive.google.com/file/d/1LBBtzzMEFdu6P6GdQfysJaB5yXHBaVu0/view?usp=drivesdk"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
